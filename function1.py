2c7cb1a01a660299a65db5f13573d09597129189
trypsr <typsr@gmail.com>
2024-05-07 17:50:33 +0900

    함수 연습1

--- function1.py
+++ function1.py
@@ -1,4 +1,59 @@
 # function1.py
+
+# 함수 이름 해석 규칙(LGB)
+# 전역변수
+x = 1
+def func(a):
+    return a + x
+#호출 
+print(func(1))
+
+# 지역변수가 있는 경우
+def func2(a):
+    x = 5
+    return a + x
+#호출 
+print(func2(1))
+
+
+# 함수의 기본값
+def times(a=10, b=20):
+    return a*b
+# 호출
+print(times())
+print(times(5))
+print(times(5, 6))
+
+#키워드인자방식(디테일기술)
+def connentURI(server, port):
+    strURL = "https://" + server + ":" + port
+    return strURL
+#호출
+print(connentURI("multi.com", "80"))
+print(connentURI(port="80", server="naver.com"))
+
+#가변인자 처리
+def union(*ar):
+    #지역변수 저장(list)
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+# 호출
+print(union("HAM", "EGG"))
+print(union("HAM", "EGG", "SPAM"))
+                    
+# 람다함수
+g = lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+
+
+
+
 #불변형식
 a = 1.2
 print("a id:", id(a) )
