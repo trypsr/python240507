63b28221ce8cc3cf5e729a9d6a3d328f8928ff05
trypsr <typsr@gmail.com>
2024-05-10 09:16:42 +0900

    4일차

--- 셀리니움_웹드라이버_네이버로그인.py
+++ 셀리니움_웹드라이버_네이버로그인.py
@@ -10,13 +10,13 @@ driver = webdriver.Chrome()
 driver.get('https://nid.naver.com/nidlogin.login')
 
 # 로그인 창에 아이디/비밀번호 입력
-loginID = "dirtyi"
+loginID = "123"
 clipboard.copy(loginID)
 #mac은 COMMAND, window는 CONTROL
 driver.find_element(By.XPATH,'//*[@id="id"]').send_keys(
     Keys.CONTROL, 'v')
 
-loginPW = "Ntrypsr2413!!"
+loginPW = "456"
 clipboard.copy(loginPW)
 driver.find_element(By.XPATH,'//*[@id="pw"]').send_keys(
     Keys.CONTROL, 'v')
